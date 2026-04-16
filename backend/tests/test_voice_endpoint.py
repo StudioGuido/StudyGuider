96ad010a4a4dcc5dc1f731d6cfc6855a9a5ebaa7
Ben Schmidt <benschmidt2005@gmail.com>
2026-04-16 19:58:11 -0400

    Fixed test_voice_endpoint.py

--- backend/tests/test_voice_endpoint.py
+++ backend/tests/test_voice_endpoint.py
@@ -6,5 +6,6 @@ def test_connect_openAI(client):
     Test connecting to the openAI realtime API using TestClient
     """
     response = client.get("/api/session")
+    data = response.json()
     assert response.status_code == 200
     assert data["client_secret"]["value"] is not None
